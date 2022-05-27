6acc8df44bb9a70346e19dc70c59a26a8caa7759
donlebo <donato.bello101@gmail.com>
2022-05-27 11:47:48 +0200

    Creazione componenti: Extra, Home, Races, Standing, Toolbar. Impostazioni delle routes per spostarsi tra i vari componenti.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExtraComponent } from './extra/extra.component';
+import { HomeComponent } from './home/home.component';
+import { RacesComponent } from './races/races.component';
+import { StandingComponent } from './standing/standing.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component : HomeComponent},
+  {path:'races', component : RacesComponent},
+  {path:'standing', component : StandingComponent},
+  {path:'extra', component : ExtraComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { RacesComponent } from './races/races.component';
+import { StandingComponent } from './standing/standing.component';
+import { ExtraComponent } from './extra/extra.component';
+import {MatTableModule} from '@angular/material/table';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    RacesComponent,
+    StandingComponent,
+    ExtraComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
